dce55b6f8a7f129e14cc3b98d054c238f4ba7e40
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-18 19:03:38 +0000

    Update TicketImplement.java

--- AbstractImplementationV1/src/TicketImplement.java
+++ AbstractImplementationV1/src/TicketImplement.java
@@ -3,18 +3,19 @@ public class TicketImplement extends AbstractTicket {
     public TicketImplement(int ticketIdNumber, double price, int seatNumber) {
         super(ticketIdNumber, price, seatNumber);
     }
-
+    
+    //Methods from abstract class have been overriden - method body is to be done over the course of the project
     @Override
     public int applyTicketDiscount() {
         // Simple output message as a method placeholder
-        System.out.println("applyTicketDiscount() called in TicketImplement. No discount applied.");
+        System.out.println("applyTicketDiscount() method called in TicketImplement class. No discount applied.");
         return 0;  
     }
 
     @Override
     public boolean checkTicketValidity() {
         // Simple output message as a method placeholder
-        System.out.println("checkTicketValidity() called in TicketImplement. Ticket is valid.");
+        System.out.println("checkTicketValidity() method called in TicketImplement class. Ticket is valid.");
         return true;
     }
 }
