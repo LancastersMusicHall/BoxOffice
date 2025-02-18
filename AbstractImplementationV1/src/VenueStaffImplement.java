b1d7aa296eec1d853efa38c81c4687a3f4ccdb33
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-18 19:04:58 +0000

    Update VenueStaffImplement.java

--- AbstractImplementationV1/src/VenueStaffImplement.java
+++ AbstractImplementationV1/src/VenueStaffImplement.java
@@ -3,16 +3,17 @@ public class VenueStaffImplement extends AbstractVenueStaff {
     public VenueStaffImplement(String name, int staffIDNumber, String role) {
         super(name, staffIDNumber, role);
     }
-
+    
+    //Methods from abstract class have been overriden - method body is to be done over the course of the project
     @Override
     public void login() {
         // Simple output message as a method placeholder
-        System.out.println("VenueStaff " + "logged in (VenueStaffImplement).");
+        System.out.println("VenueStaff logged in (VenueStaffImplement class).");
     }
 
     @Override
     public void logout() {
         // Simple output message as a method placeholder
-        System.out.println("VenueStaff " + "logged out (VenueStaffImplement).");
+        System.out.println("VenueStaff logged out (VenueStaffImplement class).");
     }
 }
