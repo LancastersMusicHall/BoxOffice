f956b684f493a36cb5511f4849daef0e9a161b57
Michael Faria <michaelronaldfaria@gmail.com>
2025-02-20 15:55:29 +0000

    Update SeatingDataInterface.java

--- InterTeamInterfacesV1/src/SeatingDataInterface.java
+++ InterTeamInterfacesV1/src/SeatingDataInterface.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 public interface SeatingDataInterface {
 
+    Seat getSeatInfo(int seatID);
+
     List<Seat> getSeatingArrangement(int hallID);
 
     void markSeatAsRestricted(int seatID);
