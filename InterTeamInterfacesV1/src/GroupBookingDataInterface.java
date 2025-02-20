4027b45001c5c3c1d56ce4608a17decea39db085
Michael Faria <michaelronaldfaria@gmail.com>
2025-02-20 15:57:26 +0000

    Update GroupBookingDataInterface.java

--- InterTeamInterfacesV1/src/GroupBookingDataInterface.java
+++ InterTeamInterfacesV1/src/GroupBookingDataInterface.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 public interface GroupBookingDataInterface {
 
+    GroupBooking getGroupBookingInfo(int groupBookingID);
+
     List<GroupBooking> getHeldSeats(int eventID);
 
     void cancelGroupBooking(int groupBookingID, int customerID);
