286d429c2bc52f3c346656dfc9a252c870d0cde6
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-21 15:59:24 +0000

    Update GroupBookingDataInterface.java

--- InterTeamInterfacesV1/src/GroupBookingDataInterface.java
+++ InterTeamInterfacesV1/src/GroupBookingDataInterface.java
@@ -1,12 +1,19 @@
 import java.util.List;
 
+//Defines methods to manage group booking data.
+//Managed by the Marketing team; prevents seats held for group bookings are not sold.
+
 public interface GroupBookingDataInterface {
-
+    
+    //Retrieves information for specific group booking.
     GroupBooking getGroupBookingInfo(int groupBookingID);
-
+    
+    //Retrieves list of group bookings (held seats) for an event.
     List<GroupBooking> getHeldSeats(int eventID);
-
+    
+    //Cancels group booking.
     void cancelGroupBooking(int groupBookingID, int customerID);
-
+    
+    //Releases any group bookings that are expired.
     void releaseExpiredHoldings();
 }
