345856833765a86242ca11e6c391a5e22d8dfadd
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-21 15:57:05 +0000

    Update FriendsDataInterface.java

--- InterTeamInterfacesV1/src/FriendsDataInterface.java
+++ InterTeamInterfacesV1/src/FriendsDataInterface.java
@@ -1,8 +1,13 @@
 import java.util.List;
 
+//Provides methods to access "Friends of Lancaster" data.
+//Managed by the Marketing team; provides priority access information to the Box Office team.
+
 public interface FriendsDataInterface {
-
+    
+    //Retrieves list of priority holders (friends) who have priority access to tickets and seats.
     List<Friend> getPriorityHolders();
-
+    
+    //Holds seats for a friend for an event.
     void holdSeatsForFriend(int friendID, int eventID);
 }
