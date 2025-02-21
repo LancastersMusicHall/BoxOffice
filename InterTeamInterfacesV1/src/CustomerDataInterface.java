915bbf2c5e4f3aa459c86a5b5b676213901c91a8
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-21 15:54:37 +0000

    Update CustomerDataInterface.java

--- InterTeamInterfacesV1/src/CustomerDataInterface.java
+++ InterTeamInterfacesV1/src/CustomerDataInterface.java
@@ -1,14 +1,22 @@
 import java.util.List;
 
+//Provides methods to access and update customer data.
+//Used by both the Operations and Marketing teams for customer insights.
+
 public interface CustomerDataInterface {
-
+    
+    //Retrieves detailed customer information.
     Customer getCustomerDetails(int customerID);
-
+    
+    //Retrieves all customers.
     List<Customer> getAllCustomers();
-
+    
+    //Retrieves list of events attended by specific customer.
     List<Event> getEventsAttendedByCustomer(int customerID);
-
+    
+    //Updates customer information.
     void updateCustomerInfo(Customer customer);
-
+    
+    //Retrieves customers with accessibility needs.
     List<Customer> getCustomersWithAccessibilityNeeds();
 }
