28ffafe7f436a4d99d1739c00e27896fd85bcd9f
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-21 16:01:24 +0000

    Update SeatingDataInterface.java

--- InterTeamInterfacesV1/src/SeatingDataInterface.java
+++ InterTeamInterfacesV1/src/SeatingDataInterface.java
@@ -1,12 +1,19 @@
 import java.util.List;
 
+//Defines methods for accessing and updating seating data.
+//Seating configurations are managed by the Operations team and reviewed by the Box Office team.
+
 public interface SeatingDataInterface {
-
+    
+    //Retrieves information for a seat.
     Seat getSeatInfo(int seatID);
-
+    
+    //Retrieves seating arrangement for a hall.
     List<Seat> getSeatingArrangement(int hallID);
-
+    
+    //Marks seat as having a restricted view.
     void markSeatAsRestricted(int seatID);
-
+    
+    //Updates the accessibility status of a seat.
     void updateSeatAccessibility(int seatID, boolean isAccessible);
 }
