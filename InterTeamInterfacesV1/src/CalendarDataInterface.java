494c1df6fc806340732ac97d66cf8cea59b928df
Meghana Gavvala <157996054+Meghana-Gavvala@users.noreply.github.com>
2025-02-21 15:52:22 +0000

    Update CalendarDataInterface.java

--- InterTeamInterfacesV1/src/CalendarDataInterface.java
+++ InterTeamInterfacesV1/src/CalendarDataInterface.java
@@ -1,10 +1,16 @@
 import java.util.List;
 
+//Defines the methods for accessing and updating calendar data. 
+//Data managed by the Operations team and used by the Box Office team for ticket sales.
+
 public interface CalendarDataInterface {
 
+    //Retrieves list of upcoming events.
     List<Event> getUpcomingEvents();
 
+    //Retrieves an event details.
     Event getEventDetails(int eventID);
-
+    
+    //Updates remaining ticket count for an event.
     void updateRemainingTickets(int eventID, int newCount);
 }
